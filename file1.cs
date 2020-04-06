62889259ac2aba94e5962ecfcbd39b5102208548
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-04-06 13:08:47 -0400

    1
    1

---
+++ file1.cs
@@ -0,0 +1,21 @@
+namespace _20200406.A
+{
+    // recipe:
+    // 1. Create the worker class
+    // 2. INVOKE the class - make an object with new
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orange o = new Orange();
+        }
+    }
+
+    class Orange
+    {
+        public void ReverseString(String a)
+        {
+            // reverse the string
+        }
+    }
+}
